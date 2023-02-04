89e3334a10440684e26ca9cea488a2ef0c0b90a7
qinchaowan <1042116744@qq.com>
2023-02-04 15:19:50 +0800

    first commit

---
+++ src/main/java/com/qcw/git/gitTest.java
@@ -0,0 +1,9 @@
+package com.qcw.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello word");
+
+    }
+}
