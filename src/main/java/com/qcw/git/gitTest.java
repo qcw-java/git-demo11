aec3b6e4dc932d5ab7cddede0e541d119fc8d3bf
qinchaowan <1042116744@qq.com>
2023-02-04 15:21:00 +0800

    second commit

--- src/main/java/com/qcw/git/gitTest.java
+++ src/main/java/com/qcw/git/gitTest.java
@@ -4,6 +4,7 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
         System.out.println("hello word");
+        System.out.println("hello word");
 
     }
 }
